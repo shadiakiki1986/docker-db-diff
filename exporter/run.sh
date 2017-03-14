47b66ebf26c49bd60f72a8c0cb19f7e24108aa30
Shadi Akiki <shadiakiki1986@gmail.com>
2017-03-14 18:56:23 +0200

    integrate email.subject. closes #3

--- exporter/run.sh
+++ exporter/run.sh
@@ -12,12 +12,24 @@ echo "export      MarketflowAcc   Marketflow..TITRE  TIT_COD"
 $PHPBIN /code/exporter/db-diff/bin/pdo-git export      MarketflowAcc   Marketflow..TITRE  TIT_COD
 
 echo "post-commit MarketflowAcc   Marketflow..TITRE ..."
-$PHPBIN /code/exporter/db-diff/bin/pdo-git post-commit MarketflowAcc   Marketflow..TITRE  --columns /code/exporter/db-diff/src/columns/ffa-titre.yml --format email --email.to s.akiki@ffaprivatebank.com --email.config /code/exporter/etc/swiftmailer/ffa.yml
+$PHPBIN /code/exporter/db-diff/bin/pdo-git post-commit \
+  MarketflowAcc   Marketflow..TITRE  \
+  --columns /code/exporter/db-diff/src/columns/ffa-titre.yml \
+  --format email \
+  --email.to s.akiki@ffaprivatebank.com \
+  --email.subject "* New securities diff - LB - `date +%Y-%m-%d`" \
+  --email.config /code/exporter/etc/swiftmailer/ffa.yml
 
 echo "export      MarketflowAccDB MkfFFADubai..TITRE TIT_COD"
 $PHPBIN /code/exporter/db-diff/bin/pdo-git export      MarketflowAccDB MkfFFADubai..TITRE TIT_COD
 
 echo "post-commit MarketflowAccDB MkfFFADubai..TITRE ..."
-$PHPBIN /code/exporter/db-diff/bin/pdo-git post-commit MarketflowAccDB MkfFFADubai..TITRE --columns /code/exporter/db-diff/src/columns/ffa-titre.yml --format email --email.to s.akiki@ffaprivatebank.com --email.config /code/exporter/etc/swiftmailer/ffa.yml
+$PHPBIN /code/exporter/db-diff/bin/pdo-git post-commit \
+  MarketflowAccDB MkfFFADubai..TITRE \
+  --columns /code/exporter/db-diff/src/columns/ffa-titre.yml \
+  --format email \
+  --email.to s.akiki@ffaprivatebank.com \
+  --email.subject "* New securities diff - AE - `date +%Y-%m-%d`" \
+  --email.config /code/exporter/etc/swiftmailer/ffa.yml
 
 echo "end"
